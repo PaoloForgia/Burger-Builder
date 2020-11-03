2b1e61cc611077d7e3896391a68b8d5dfd86811f
paolo.forgia <paolo.forgia@appway.com>
2020-11-03 15:09:51 +0100

    Form validation

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,98 @@
+import React from "react";
+import classes from "./Input.css";
+
+const input = (props) => {
+  let inputElement = null;
+  const inputClasses = [classes.InputElement];
+
+  if (props.touched && props.invalid && props.shouldValidate) {
+    inputClasses.push(classes.Invalid);
+  }
+
+  switch (props.elementType) {
+    case "input":
+      inputElement = (
+        <input
+          onChange={props.changed}
+          className={inputClasses.join(" ")}
+          {...props.elementConfig}
+          value={props.value}
+        />
+      );
+      break;
+    case "textarea":
+      inputElement = (
+        <textarea
+          onChange={props.changed}
+          className={inputClasses.join(" ")}
+          {...props.elementConfig}
+          value={props.value}
+        />
+      );
+      break;
+    case "select":
+      inputElement = (
+        <select
+          onChange={props.changed}
+          className={inputClasses.join(" ")}
+          value={props.value}
+        >
+          {props.elementConfig.options.map((option) => (
+            <option value={option.value}>{option.displayValue}</option>
+          ))}
+        </select>
+      );
+      break;
+    default:
+      inputElement = (
+        <input
+          onChange={props.changed}
+          className={inputClasses.join(" ")}
+          {...props.elementConfig}
+          value={props.value}
+        />
+      );
+      break;
+  }
+
+  return (
+    <div className={classes.Input}>
+      <label className={classes.Label}>{props.label}</label>
+      {inputElement}
+    </div>
+  );
+};
+
+export const inputProperties = (
+  elementType,
+  contentType,
+  placeholder,
+  validation,
+  defaultValue
+) => {
+  return {
+    elementType: elementType,
+    elementConfig: {
+      type: contentType,
+      placeholder: placeholder,
+    },
+    validation: validation,
+    valid: false,
+    touched: false,
+    value: defaultValue,
+  };
+};
+
+export const selectProperties = (elementType, options, defaultValue) => {
+  return {
+    elementType: elementType,
+    elementConfig: {
+      options: options,
+    },
+    validation: {},
+    valid: true,
+    value: defaultValue,
+  };
+};
+
+export default input;
